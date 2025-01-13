8287a66304439f3d62e959771cd8d06eb59e0fb9
Alexsander Rjevskiy <porezkakir2@gmail.com>
2025-01-13 20:41:54 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,20 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Loader from './components/Loader';
+import Navigation from './components/Navigation'; 
 
 const HomePage = React.lazy(() => import('./pages/HomePage'));
 const MoviesPage = React.lazy(() => import('./pages/MoviesPage'));
 const MovieDetailsPage = React.lazy(() => import('./pages/MovieDetailsPage'));
 const NotFoundPage = React.lazy(() => import('./pages/NotFoundPage'));
 
-// Lazy loading for MovieCast and MovieReviews
 const MovieCast = React.lazy(() => import('./components/MovieCast'));
 const MovieReviews = React.lazy(() => import('./components/MovieReviews'));
 
 const App = () => {
   return (
-    <Router>
-      <header className="header">
-        <nav>
-          <button className="nav-button">
-            <Link to="/">Home</Link>
-          </button>
-          <button className="nav-button">
-            <Link to="/movies">Movies</Link>
-          </button>
-        </nav>
-      </header>
+    <>
+      <Navigation />
 
       <main>
         <Suspense fallback={<Loader />}>
@@ -38,11 +29,12 @@ const App = () => {
           </Routes>
         </Suspense>
       </main>
-    </Router>
+    </>
   );
 };
 
 export default App;
+
 
 
 
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css'; 
+
+const Navigation = () => {
+  return (
+    <header className="navigation">
+      <nav>
+        <button className="nav-button">
+          <Link to="/">Home</Link>
+        </button>
+        <button className="nav-button">
+          <Link to="/movies">Movies</Link>
+        </button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
